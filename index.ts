fa172c08bf85fb6b02a9cd4922560bc968392607
xprilion <xprilion@gmail.com>
2024-03-19 16:08:37 +0530

    updates

--- index.ts
+++ index.ts
@@ -1,15 +1,40 @@
 const puppeteer = require('puppeteer');
 const { PuppeteerScreenRecorder } = require('puppeteer-screen-recorder');
 
+const Config = {
+    followNewTab: true,
+    fps: 60,
+    videoFrame: {
+      width: 1920,
+      height: 1080,
+    },
+    videoCrf: 18,
+    videoCodec: 'libx264',
+    videoPreset: 'ultrafast',
+    videoBitrate: 1000,
+    aspectRatio: '16:9',
+  };
+
 (async () => {
-    const browser = await puppeteer.launch();
+
+    const browser = await puppeteer.launch({
+        headless: true,
+      });
+
     const page = await browser.newPage();
-    const recorder = new PuppeteerScreenRecorder(page);
-    await page.goto('https://www.w3schools.com/css/tryit.asp?filename=trycss3_animation5');
-    await recorder.start('recordings/recording.mp4'); // Specify the path and file name for the recording.
+    await page.setViewport({
+        width: 1920,
+        height: 1080,
+        deviceScaleFactor: 1,
+    });
+
+    const recorder = new PuppeteerScreenRecorder(page, Config);
+    await page.goto('https://xprilion.com');
+    await recorder.start('recordings/recording.webm');
     
     await new Promise(_func=> setTimeout(_func, 5000));
     
     await recorder.stop();
+    await page.close();
     await browser.close();
 })();
